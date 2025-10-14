a9eab8db4b3c3c46c855cd28bb79a7a7515ff18a
JohnTigerino <johntigerino2103@gmail.com>
2025-10-14 02:59:36 -0600

    Cuarta pantalla y pequeños detalles

---
+++ app/src/main/java/com/bionica/visor_prueba3/AnalisisActivity.kt
@@ -0,0 +1,11 @@
+package com.bionica.visor_prueba3
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AnalisisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analisis)
+
+    }
+}
